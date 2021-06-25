367e173953542a03d22dd87ee528657f3e932790
Rouyem <rouyemilie82@gmail.com>
2021-06-25 10:12:09 +0200

    inscription

--- Controller/InscriptionController.php
+++ Controller/InscriptionController.php
@@ -1,11 +1,48 @@
 <?php
 namespace App\Controller;
 
+use App\Core\Form;
+use App\Models\UtilisateursModel;
+
 class InscriptionController extends Controller
 {    
     public function index(){
 
         $this->render('inscription/index');
         
+    }
+
+    //inscription
+    public function inscription()
+    {
+        if(Form::validate($_POST, ['nom', 'prenom', 'adresse', 'tel', 'email', 'mdp', 'mdp2']))
+        {
+            $nom = ($_POST['nom']);
+            $prenom = ($_POST['prenom']);
+            $adresse = ($_POST['adresse']);
+            $tel = ($_POST['tel']);
+            $role = 'ROLE_USER';
+            $date = date('Y-m-d H:i:s');
+            // On nettoie l'e-mail pour éviter les failles XSS et on chiffre le mot de passe
+            $email = strip_tags($_POST['email']);
+            $mdp = password_hash($_POST['mdp'], PASSWORD_ARGON2I);
+            //création utilisateur
+            $newUser = new UtilisateursModel();
+            $newUser->setNom($nom)
+                    ->setPrenom($prenom)
+                    ->setAdresse($adresse)
+                    ->setTel($tel)
+                    ->setEmail($email)
+                    ->setMdp($mdp)
+                    ->setRole($role)
+                    ->setDate_creation($date);
+            //$newUser['role'] = !isset($newUser['role']) ? 'ROLE_USER' : $newUser['role'];
+            $newUser->create();
+
+            header('Location: ../Compte');
+        }
+        else{
+            echo 'Veuillez compléter tous les champs';
+        }
     }
 }
--- views/inscription/index.php
+++ views/inscription/index.php
@@ -5,7 +5,7 @@
 </div>
 
 
-<form action="" class="form2" method="post">
+<form action="/Inscription/inscription" class="form2" method="post">
     <div class="form2__container">
         <div class="form2__flex">
             <div class="form2__content">
@@ -19,32 +19,29 @@
         </div>
         <div class="form2__content">
             <label for="">Adresse :</label>
-            <input type="text" name="" id="" require minlength="1">
+            <input type="text" name="adresse" id="adresse" require minlength="1">
         </div>
         <div class="form2__flex">
             <div class="form2__content">
                 <label for="">Téléphone :</label>
-                <input type="text" name="" id="" require maxlength="10" require minlength="10">
+                <input type="text" name="tel" id="tel" require maxlength="10" require minlength="10">
             </div>
             <div class="form2__content">
                 <label for="">Email :</label>
-                <input type="email" name="" id="" require minlength="3" require maxlength="64">
+                <input type="email" name="email" id="email" require minlength="3" require maxlength="64">
             </div>
         </div>
         <div class="form2__flex">
             <div class="form2__content">
                 <label for="">Mot de passe :</label>
-                <input type="password" name="" id="">
+                <input type="password" name="mdp" id="mdp">
             </div>
             <div class="form2__content">
                 <label for="">Confirmer mot de passe :</label>
-                <input type="password" name="" id="">
+                <input type="password" name="mdp2" id="mdp2">
             </div>
         </div>
-        <div class="form2__content">
-            <label for="">Plaque d'immatriculation :</label>
-            <input type="text" name="" id="">
-        </div>
+        
 
         <div class="form2__btn">
             <button type="submit">Envoyer</button>
