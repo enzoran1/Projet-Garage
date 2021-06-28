9c18bf7482217f224416c96d7ba0aa148a7743b5
Martin <martin.aubertin@gmail.com>
2021-06-28 17:37:41 +0200

    ajout d'une condition permettant uniquement à l'admin de pénétrer dans l'interface

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -5,6 +5,15 @@ class AdminController extends Controller
 {
   public function index()
   {
-    return $this->render('admin/index');
+    if($_SESSION['user']['role'] !== 'ROLE_ADMIN')
+    { 
+      // renvoyer une erreur, chercher le code 
+      echo 'Veuillez contacter l\'administrateur système';
+      return $this->render('main/index');
+    } 
+    else 
+    { 
+      return $this->render('admin/index');
+    }
   }
 }
