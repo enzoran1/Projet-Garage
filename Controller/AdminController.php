f80d84139261da9bd88be43a69878ecc094eaecd
Alexandre Colas <alexandre_colas@orange.fr>
2021-06-29 14:39:48 +0200

    aaa

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -1,57 +1,54 @@
 <?php
+
 namespace App\Controller;
+
 use App\Models\ClientModel;
 use App\Models\MessageModel;
+
 
 class AdminController extends Controller
 {
   public function index()
   {
-    if(empty($_SESSION) || $_SESSION['user']['role'] !== 'ROLE_ADMIN')
-    { 
+    if (empty($_SESSION) || $_SESSION['user']['role'] !== 'ROLE_ADMIN') {
       // renvoyer une erreur, chercher le code 
       return $this->render('main/index');
-    } 
-    else 
-    { 
+    } else {
       return $this->render('admin/index');
     }
   }
 
-  public function show(){
+  public function show()
+  {
 
-    if(empty($_SESSION) || $_SESSION['user']['role'] !== 'ROLE_ADMIN')
-    { 
-     // renvoyer une erreur, chercher le code 
-     return $this->render('main/index');
-    } 
+    if (empty($_SESSION) || $_SESSION['user']['role'] !== 'ROLE_ADMIN') {
+      // renvoyer une erreur, chercher le code 
+      return $this->render('main/index');
+    }
 
     //on instancie le modéle correspondant a la table 'utilisitateur
 
     $utilisateurModel = new ClientModel;
-    
+
     // on va chercher toutes les utilisateur
 
     $utilisateur = $utilisateurModel->findAll();
 
     // On génére la vue 
-    $this->render('admin/show/index', compact('utilisateur'));  
+    $this->render('admin/show/index', compact('utilisateur'));
   }
 
   public function annonce()
-  { 
+  {
     // silence is golden
   }
 
   public function message()
-  { 
-    if(empty($_SESSION) || $_SESSION['user']['role'] !== 'ROLE_ADMIN')
-    { 
+  {
+    if (empty($_SESSION) || $_SESSION['user']['role'] !== 'ROLE_ADMIN') {
       // renvoyer une erreur, chercher le code 
       return $this->render('main/index');
-    }
-    else
-    {
+    } else {
       // instancier le model 
       $messageModel = new MessageModel;
 
@@ -59,7 +56,6 @@ class AdminController extends Controller
       $messages = $messageModel->findAll();
       // render la view
       return $this->render('admin/message/index', compact('messages'));
-      
     }
   }
 }
