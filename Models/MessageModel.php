7a919ac87d24aa28a68d0e123bd3f3fa2db0da68
Enzo Franceschini <enzo.franceschinidev@gmail.com>
2021-06-29 10:06:34 +0200

    ajout admin

---
+++ Models/MessageModel.php
@@ -0,0 +1,121 @@
+<?php
+namespace App\Models;
+
+class MessageModel extends Model
+{   
+    
+    protected $nom;
+    protected $prenom;
+    protected $email;
+    protected $date;
+    protected $message;
+    
+    
+
+
+    public function __construct()
+    {
+        $this->table = 'contact';
+    }
+
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of date
+     */ 
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the value of date
+     *
+     * @return  self
+     */ 
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of message
+     */ 
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set the value of message
+     *
+     * @return  self
+     */ 
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
--- views/admin/index.php
+++ views/admin/index.php
@@ -1,2 +1,38 @@
-<h1>Page Admin</h1>
+
+<div class="ban" id="ban__admin">
+<h1>Administration</h1>
+</div>
+
+<div class="admin__container">
+<div class="admin-flex">
+    <div class="admin__content">
+        <img src="image/admin-utilisateur.png" alt="">
+        <h3>Mes utilisateurs</h3>
+        <a href="/client">Accéder</a>
+    </div>
+
+    <div class="admin__content">
+        <img src="image/admin-calender.png" alt="">
+        <h3>Mes rendez-vous</h3>
+        <a href="">Accéder</a>
+    </div>
+</div>
+<div class="admin-flex">
+    <div class="admin__content">
+        <img src="image/admin-message.png" alt="">
+        <h3>Mes messages</h3>
+        <a href="/message">Accéder</a>
+    </div>
+
+    <div class="admin__content">
+        <img src="image/admin-annonce.png" alt="">
+        <h3>Mes annonces</h3>
+        <a href="">Accéder</a>
+    </div>
+</div>
+</div>
+
+
+
 <a href="Compte/logout">DECONNEXION</a>
+
