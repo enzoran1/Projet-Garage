ec3f5f448f7f8d5290e539bc4a0bf99354c9983d
Martin <martin.aubertin@gmail.com>
2021-07-01 17:02:32 +0200

    indentation

--- views/compte/dashboard/index.php
+++ views/compte/dashboard/index.php
@@ -3,7 +3,6 @@
 <div class="ban" id="ban__dashboard">
     <h1>Mon profil</h1>
 </div>
-<div>
 
 <div class="utilisateur2__flex-container">
 
@@ -11,6 +10,7 @@
         <div class="utilisateur2__titre">
             <h3>Mes information</h3>
         </div>
+
         <div class="carte__utilisateur2-container">
             <div class="utilisateur2__flex-paire">
                 <div class="utilisateur2__flex">
@@ -44,9 +44,10 @@
             </div>
         </div>
     </div>
-<div id="modal-pub" class="modal hidden">
+    
+    <div id="modal-pub" class="modal hidden">
         <div class="conteneur-modal">
-            <div class="close">&times;</div>
+            <div class="close">&times; </div>
             <form class="form__modal" action="/compte/editProfileView" method="post">
             <div class="modal__content">
                 <label for="">mot de passe</label>
@@ -63,28 +64,31 @@
         </div>
     </div>
 
-<div class="utilisateur2__véhicule">
-    <h3>Véhicules</h3>
-    <?php foreach ($vehicules as $vehicule) : ?>
-    <p>Plaque d'immatriculation : </p>
-    <p><?= $vehicule->plaque_immatriculation ?></p>
-    <p>Marque : </p>
-    <p><?= $vehicule->marque ?></p>
-    <p>Modèle : </p>
-    <p><?= $vehicule->modele ?></p>
-    <p>Motorisation : </p>
-    <p><?= $vehicule->motorisation ?></p>
-    <p>Type de véhicule : </p>
-    <p><?= $vehicule->type_vehicule ?></p>
-    <p>Annee : </p>
-    <p><?= $vehicule->annee ?></p>
-    <p>Kilomètre : </p>
-    <p><?= $vehicule->km ?></p>
-    <?php endforeach;   ?>
+    <div class="utilisateur2__véhicule">
+        <h3>Véhicules</h3>
+        <?php foreach ($vehicules as $vehicule) : ?>
+        <p>Plaque d'immatriculation : </p>
+        <p><?= $vehicule->plaque_immatriculation ?></p>
+        <p>Marque : </p>
+        <p><?= $vehicule->marque ?></p>
+        <p>Modèle : </p>
+        <p><?= $vehicule->modele ?></p>
+        <p>Motorisation : </p>
+        <p><?= $vehicule->motorisation ?></p>
+        <p>Type de véhicule : </p>
+        <p><?= $vehicule->type_vehicule ?></p>
+        <p>Annee : </p>
+        <p><?= $vehicule->annee ?></p>
+        <p>Kilomètre : </p>
+        <p><?= $vehicule->km ?></p>
+        <?php endforeach;   ?>
 
-    <a href="compte/ajoutVehicule"><img src="image\icons8-plus-100.png" alt=""></a>       
-</div> 
+        <a href="compte/ajoutVehicule">
+            <img src="image\icons8-plus-100.png" alt="">
+        </a>       
+    </div> 
 </div>
+
 <div class="utilisateur2__btn">
     <a id="utilisateur2__btn-color" href="Compte/logout">Deconnexion</a>
-  </div>
+</div>
