cb4591c91188cfa2953d3b641b435079a67ee146
Martin <martin.aubertin@gmail.com>
2021-07-01 15:33:34 +0200

    resolution probleme modal, plus d'erreur js, il est appellé uniquement dans l apage concernée via un onclick

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,64 +1,66 @@
-
-// constante qui représente le bouton "modifier"
-const afficheModal = document.querySelector('#affiche-modal');
-
-// correspond au modal en intégralité, caché par défaut
-const modalPub = document.querySelector('#modal-pub');
-
-// bouton submit
-const submitButton = document.getElementById('submitpasswordmodal');  
-
-
-const pass1 = document.getElementById('password1');
-const pass2 = document.getElementById('password2');  
-
-
-// évènement -> au click, le bouton enleve le hidden et ajoute le flou
-afficheModal.addEventListener('click', (e) => {
-    modalPub.classList.remove('hidden');
-})
-
-
-const listeModal = document.querySelectorAll('.modal').forEach((modal) => 
+function modal()
 {
 
-    // contenu du modal 
-    const conteneurModal = modal.querySelector('.conteneur-modal');
+    // constante qui représente le bouton "modifier"
+    const afficheModal = document.querySelector('#affiche-modal');
 
-    // bouton pour fermer le modal
-    const modalPubClose = modal.querySelector('.close');
+    // correspond au modal en intégralité, caché par défaut
+    const modalPub = document.querySelector('#modal-pub');
 
-    // ajout event : ferme le modal si on appuie
-    modalPubClose.addEventListener('click', cacheModal);
+    // bouton submit
+    const submitButton = document.getElementById('submitpasswordmodal');  
 
-    // ajout event, au click, cache le modal
-    modal.addEventListener('click', cacheModal);
 
-    submitButton.addEventListener('click', testUser);
+    const pass1 = document.getElementById('password1');
+    const pass2 = document.getElementById('password2');  
 
-    // éviter la propagation sur les parents, enfants
-    conteneurModal.addEventListener('click', (e) => { e.stopPropagation() });
 
-    // ajoute le hidden 
-    function cacheModal(e) 
+    // évènement -> au click, le bouton enleve le hidden et ajoute le flou
+    afficheModal.addEventListener('click', (e) => {
+        modalPub.classList.remove('hidden');
+    })
+
+
+    const listeModal = document.querySelectorAll('.modal').forEach((modal) => 
     {
-        modal.classList.add('hidden');
-        e.stopPropagation();
-    }
 
-    function testUser() // cette méthode vérifie que les deux mots de passe renseignés sont identifiques 
-    {  
+        // contenu du modal 
+        const conteneurModal = modal.querySelector('.conteneur-modal');
 
-        if(pass1.value === pass2.value)
-        { 
-            return true;
+        // bouton pour fermer le modal
+        const modalPubClose = modal.querySelector('.close');
+
+        // ajout event : ferme le modal si on appuie
+        modalPubClose.addEventListener('click', cacheModal);
+
+        // ajout event, au click, cache le modal
+        modal.addEventListener('click', cacheModal);
+
+        submitButton.addEventListener('click', testUser);
+
+        // éviter la propagation sur les parents, enfants
+        conteneurModal.addEventListener('click', (e) => { e.stopPropagation() });
+
+        // ajoute le hidden 
+        function cacheModal(e) 
+        {
+            modal.classList.add('hidden');
+            e.stopPropagation();
         }
-        else
-        { 
-            alert('les deux mots de passes ne sont pas identiques, veuillez réessayer');
-            pass1.value = "";
-            pass2.value = "";
-        }
-    }
-});
 
+        function testUser() // cette méthode vérifie que les deux mots de passe renseignés sont identifiques 
+        {  
+
+            if(pass1.value === pass2.value)
+            { 
+                return true;
+            }
+            else
+            { 
+                alert('les deux mots de passes ne sont pas identiques, veuillez réessayer');
+                pass1.value = "";
+                pass2.value = "";
+            }
+        }
+    });
+}
