0b40653700bf26b08b7e4e7cc3c3aa85e80537cf
Alexandre Colas <alexandre_colas@orange.fr>
2021-07-12 09:20:24 +0200

    modif enzo

--- views/admin/annonces/index.php
+++ views/admin/annonces/index.php
@@ -4,44 +4,58 @@
 <a href="/Admin/ajoutAnnoncesFrom">ajout</a>
 
 <div class="admin-annonces-grid">
-<?php foreach($annonces as $annonce) : ?>
+    <?php foreach ($annonces as $annonce) : ?>
 
- <div class="admin-annonces">
-    
-    <div class="admin-annonces__container">
-        <form enctype="multipart/form-data" action="/admin/ajouterPhoto/<?=$annonce->id?>" method="POST">
-           <input class="input-file" type="file" name="photo" id="photo"  accept="image/png, image/jpeg">
-           <button type="submit">Ajouter photo</button>
-        </form>
-        <div class="admin-annonces__content">
-            <p>marque : <?=$annonce->lib_marque?></p>
-            <p>prix : <?=$annonce->prix?>€</p>
-        </div>
-        <div class="admin-annonces__content-description">
-            <p><?=$annonce->description?></p>
-        </div>
-        <div class="admin-annonces__content">
-            <p>km : <?=$annonce->km?>km</p>
-            <p>annee : <?=$annonce->annee?></p>
-        </div>
-        <div class="admin-annonces__content">
-            <p>motorisation : <?=$annonce->lib_motorisation?></p>
-            <p>type : <?=$annonce->lib_type?></p>
-        </div>
-        <div class="admin-annonces__content-immatriculation">
-            <p>immatriculation : <?=$annonce->plaque_immatriculation?></p>
+        <div class="admin-annonces">
+
+            <div class="admin-annonces__container">
+                <form enctype="multipart/form-data" action="/admin/ajouterPhoto/<?= $annonce->id ?>" method="POST">
+                    <input class="input-file" type="file" name="photo" id="photo" accept="image/png, image/jpeg">
+                    <button type="submit">Ajouter photo</button>
+                </form>
+                <div>
+                    <p>Photo : </p>
+
+                    <?php
+                    // var_dump($_SESSION);
+                    // die();
+
+                    if (file_exists("/image/" . $_SESSION['photo']) && isset($_SESSION['photo'])) {
+                    ?>
+                        <img src="<?= "/image/" . $_SESSION['photo']; ?>" style="width: 65%" />
+
+                    <?php
+                    }
+                    ?>
+
+                </div>
+                <div class="admin-annonces__content">
+                    <p>marque : <?= $annonce->lib_marque ?></p>
+                    <p>prix : <?= $annonce->prix ?>€</p>
+                </div>
+                <div class="admin-annonces__content-description">
+                    <p><?= $annonce->description ?></p>
+                </div>
+                <div class="admin-annonces__content">
+                    <p>km : <?= $annonce->km ?>km</p>
+                    <p>annee : <?= $annonce->annee ?></p>
+                </div>
+                <div class="admin-annonces__content">
+                    <p>motorisation : <?= $annonce->lib_motorisation ?></p>
+                    <p>type : <?= $annonce->lib_type ?></p>
+                </div>
+                <div class="admin-annonces__content-immatriculation">
+                    <p>immatriculation : <?= $annonce->plaque_immatriculation ?></p>
+                </div>
+
+                <div class="admin-annonces__btn">
+                    <a href="">Modifier</a>
+                    <a href="/admin/supprimerAnnonces/<?= $annonce->id ?>">Supprimer</a>
+                </div>
+            </div>
         </div>
 
-        <div class="admin-annonces__btn">
-            <a href="">Modifier</a>
-            <a href="/admin/supprimerAnnonces/<?=$annonce->id?>">Supprimer</a>
-        </div>
-    </div>
+
+
+    <?php endforeach; ?>
 </div>
-
-
-    
-<?php endforeach; ?>
-</div>
-    
-
