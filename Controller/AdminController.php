8ea62c7f9bb6bfb95bd70ae9d6b90e62f86eb3cd
enzoran1 <enzo.franceschinidev@gmail.com>
2021-07-12 16:33:14 +0200

    ajàut front prestation pour l'utilisateur

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -198,7 +198,7 @@ public function supprimerUtilisateur(int $id)
   }
 
 
-  public function annonces()
+  public function annonces(int $id)
   {
     if (empty($_SESSION) || $_SESSION['user']['role'] !== 'ROLE_ADMIN') {
       // renvoyer une erreur, chercher le code 
@@ -206,19 +206,26 @@ public function supprimerUtilisateur(int $id)
     } else {
       // instancier le model 
       $annoncesModel = new AnnoncesModel;
+      $photoModel = new PhotoModel;
       $requete = $annoncesModel->requete('SELECT a_vendre.*, marque.lib_marque, motorisation.lib_motorisation, type_vehicule.lib_type
       FROM a_vendre
       INNER JOIN marque ON a_vendre.id_marque = marque.id
       INNER JOIN motorisation ON a_vendre.id_motorisation = motorisation.id 
       INNER JOIN type_vehicule ON a_vendre.id_type = type_vehicule.id_type
+    
       order by lib_marque asc');
       // il faut ajouter la putain de photo :'(  
+        // requete pour lié les photo et l'annonce 
+      // $requete2 = $photoModel->requete('SELECT * FROM photo WHERE id_avendre ='.$id)
 
       // méthode 
       $annonces = $requete->fetchAll();
+     
+
+      $photo = $photoModel->findAll();
 
       // render la view
-      return $this->render('admin/annonces/index', compact('annonces'));
+      return $this->render('admin/annonces/index', compact('annonces', 'photo'));
     }
   }
 
@@ -355,11 +362,13 @@ public function ajoutAnnonces()
       move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile);
     }
     $newPhoto = new PhotoModel();
-    $newPhoto->setLib_photo($uploadfile)
+    $newPhoto->setLib_photo(str_replace('../public', '',$uploadfile))
              ->setId_avendre($id);
     $newPhoto->create();
     header('Location: /admin/annonces');
   }
+
+
 
 
 
--- views/admin/annonces/index.php
+++ views/admin/annonces/index.php
@@ -13,6 +13,11 @@
     <div class="admin-annonces__container">
         <form enctype="multipart/form-data" action="/admin/ajouterPhoto/<?=$annonce->id?>" method="POST">
            <input class="input-file" type="file" name="photo" id="photo"  accept="image/png, image/jpeg">
+           <?php foreach($photo as $photos) :?>
+            <p><?=$photos->id_avendre?></p>
+            <img src="<?=$photos->lib_photo?>" alt="">
+            
+            <?php endforeach;?>
            <button type="submit">Envoyer photo</button>
         </form>
         <div class="admin-annonces__content">
