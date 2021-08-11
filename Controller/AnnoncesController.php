b4bf2292ddd4270dd0945647be28e3e216d41acb
Martin A <martin.aubertin@gmail.com>
2021-08-11 14:46:41 +0200

    indentation annonceController

--- Controller/AnnoncesController.php
+++ Controller/AnnoncesController.php
@@ -6,42 +6,41 @@ use App\Models\AnnoncesModel;
 use App\Models\PhotoModel;
 
 class AnnoncesController extends Controller
-{
-   
-    public function index()
-    {
-
-    // instancier le model 
+{  
+  public function index()
+  { // instancier le model 
     $annoncesModel = new AnnoncesModel;
     $photoModel = new PhotoModel;
-    $requete = $annoncesModel->requete('SELECT a_vendre.*, marque.lib_marque, motorisation.lib_motorisation, type_vehicule.lib_type
-    FROM a_vendre
-    INNER JOIN marque ON a_vendre.id_marque = marque.id
-    INNER JOIN motorisation ON a_vendre.id_motorisation = motorisation.id 
-    INNER JOIN type_vehicule ON a_vendre.id_type = type_vehicule.id_type
-  
-    order by lib_marque asc');
-
+    $requete = $annoncesModel->requete
+    (
+      'SELECT a_vendre.*, marque.lib_marque, motorisation.lib_motorisation, type_vehicule.lib_type
+      FROM a_vendre
+      INNER JOIN marque ON a_vendre.id_marque = marque.id
+      INNER JOIN motorisation ON a_vendre.id_motorisation = motorisation.id 
+      INNER JOIN type_vehicule ON a_vendre.id_type = type_vehicule.id_type
+      order by lib_marque asc'
+    );
     $annonces = $requete->fetchAll();
-   
+
 
     $photosBdd = $photoModel->findAll();
 
-
     $photos=[];
 
-    foreach($photosBdd as $photoBdd) {
-      if(!isset($photos[$photoBdd->id_avendre])) {
+    foreach($photosBdd as $photoBdd) 
+    {
+      if(!isset($photos[$photoBdd->id_avendre])) 
+      {
         $photos[$photoBdd->id_avendre] = [];
       }
+
       $photos[$photoBdd->id_avendre][] = $photoBdd;
-
     }
 
 
-        // On génére la vue 
-        $this->render('annonces/index', compact('annonces', 'photos'));
-    }
+    // On génére la vue 
+    $this->render('annonces/index', compact('annonces', 'photos'));
+  }
 }
 
 
